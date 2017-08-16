fe4308fc0cf2df05d17e8c34c97bc1b2240dccc4
sbcreates <drawsarahb@gmail.com>
2017-08-16 11:36:36 -0400

    Finished part one of exercise 0. Runs OK

---
+++ exercise0.rb
@@ -0,0 +1,12 @@
+fav_colors = ["blue", "green", "white", "black", "gray"]
+
+family_ages = [30, 32, 28, 47, 45, 68, 67]
+
+5.times do |coin_flip|
+  coin_flip = ["heads", "tails"].sample
+  p coin_flip
+end
+
+fav_artists = ["Haim", "NSYNC", "Every Time I Die"]
+
+fav_colors = [:blue, :green, :white, :black, :gray]
